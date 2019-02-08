111cede2925a4ddeeeab0b5880cd5ae226a03793
chdu <duchenpaul@gmail.com>
2019-02-08 08:16:00 +0800

    Add extra key test

--- phone_dict_gen.py
+++ phone_dict_gen.py
@@ -26,14 +26,22 @@ def generate_dict(phone_num_region):
 @logging_manager.logging_to_file
 def generate_dict_8_digits():
     digits = 8
-    with open('phone.dict', 'w') as f:
-        f.write('')
+
+    add_number = r'''
+    1234567890
+    123456789
+    '''
+    with open('8_digits.dict', 'w') as f:
+        f.write('\n'.join([x for x in add_number.split('\n') if x]))
+
+    with open('8_digits.dict', 'a') as f:
+        f.write('\n')
         for j in range(10**digits):
             number = str(j).zfill(digits)
             f.write(number + '\n')
+
 
 if __name__ == '__main__':
     phone_num_region = query_phone_num_region()
     generate_dict(phone_num_region)
     # generate_dict_8_digits()
-
