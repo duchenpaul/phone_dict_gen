24b69f3ef7e7d2cc045331a1d2b323bbae52407b
Chenny Du <duchenpaul@gmail.com>
2024-11-16 23:10:59 +0800

    add conditional checks for hc22000 hash file generation and hashcat session restoration

--- crack_script/run_hashcat.sh
+++ crack_script/run_hashcat.sh
@@ -24,19 +24,33 @@ echo "Dictionary file: ${dict_path}"
 
 cap_name=$(basename ${cap} .cap)
 
-echo "Generate hc22000 hash file"
 hc22000_file=${cap_path}/${cap_name}.hc22000
-hcxpcapngtool -o ${hc22000_file} ${cap_path}/${cap_name}.cap 
-
-echo "Generated: "
-ls -l ${hc22000_file}
+if [ -f ${hc22000_file} ]; then
+    echo "Generate hc22000 hash file"
+    hcxpcapngtool -o ${hc22000_file} ${cap_path}/${cap_name}.cap 
+    echo "Generated: "
+    ls -l ${hc22000_file}
+else
+    echo "hc22000 hash file found, skip generating"
+fi
 
 echo "Show phone dictionary"
 ls -l ${dict_path}
 
-echo "Kicking off hashcat..."
-hashcat -m 22000 -w 3 --hwmon-disable \
---session ${cap_name} --status --status-timer 10 \
-${hc22000_file} \
-${dict_path} \
--o ${work_path}/${cap_name}_key.txt --potfile-path ${work_path}/${cap_name}.potfile
+if [ -f ${work_path}/${cap_name}.restore ]; then
+    echo "Restore file exists..."
+    hashcat --session ${cap_name} \
+     -w 4 \
+    --restore \
+    --restore-file-path=${work_path}/${cap_name}.restore
+else
+    echo "Kicking off hashcat..."
+    hashcat -m 22000 -w 3 --hwmon-disable \
+    --session ${cap_name} --status --status-timer 10 \
+    --restore-file-path=${work_path}/${cap_name}.restore \
+    ${hc22000_file} \
+    ${dict_path} \
+    -o ${work_path}/${cap_name}_key.txt --potfile-path ${work_path}/${cap_name}.potfile
+fi
+
+printf "$0 finished at `date`\n"
